f7f53d8e4223c96dc2b4a52a00fa8999557e91fe
Thiago Almeida <tapgyn@gmail.com>
2012-06-22 11:16:14 -0300

    Select a Course. 221 examples, 0 failures, 19 pending

--- spec/views/posts/_new_post.html.erb_spec.rb
+++ spec/views/posts/_new_post.html.erb_spec.rb
@@ -5,10 +5,12 @@ describe "posts/_new_post" do
     view.stub!(:topic_user).and_return(nil)
     view.stub!(:current_user).and_return(current_user_master)
     render
+    assert_select "select#topic_user_id", text: "Select a Course"
   end
 
   it "renders with hidden tag" do
     view.stub!(:topic_user).and_return(Fabricate(:topic_user))
     render
+    assert_select "input#topic_user_id", type: "hidden"
   end
 end
