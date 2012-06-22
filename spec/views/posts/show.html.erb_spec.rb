a198da5d8910f2002187185ed5f2ed15983c671c
Thiago Almeida <tapgyn@gmail.com>
2012-06-22 18:00:48 -0300

    _post view links for actions styled

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -9,7 +9,7 @@ describe "posts/show" do
     ))
   end
 
-  it "renders attributes in <p>" do
+  it "renders" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(//)
