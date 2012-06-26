2e904746a3b352b5d27cfad6e06745956a0e9dfa
Thiago Almeida <tapgyn@gmail.com>
2012-06-26 16:45:30 -0300

    like model tested. 276 examples, 0 failures, 26 pending

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,44 @@
 require 'spec_helper'
 
 describe Like do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    describe "understands" do
+      {user: User, likable: ActiveRecord::Base}.each do |assoc, clazz|
+        it assoc do
+          like = Fabricate(:like)
+          like.should respond_to(assoc)
+          like.send(assoc).should be_a(clazz)
+        end
+      end
+    end
+    describe "validating polymorphic likable" do
+      %w(post reply).each do |likable_type|
+        it "#{likable_type} is likable" do
+          likable = Fabricate(likable_type)
+          -> {
+            Fabricate(:like, likable: likable)
+          }.should change(likable.likes, :count).by(1)
+        end
+      end
+    end
+  end
+
+  describe "field validations" do
+    it "user_id is unique per likable" do
+      user = Fabricate(:user)
+      post = Fabricate(:post)
+
+      -> {
+        Fabricate(:like, user: user, likable: post)
+      }.should change(post.likes, :count).by(1)
+      
+      -> {
+        Fabricate(:like, user: user, likable: post)
+      }.should raise_error(ActiveRecord::RecordInvalid)
+
+      like = Fabricate.build(:like, user: user, likable: post)
+      like.should_not be_valid
+      like.errors[:user_id].should_not be_empty
+    end
+  end
 end
