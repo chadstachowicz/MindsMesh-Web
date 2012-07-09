dc4c8827f286a82d88246a678b8f32bbbb34d662
Thiago Almeida <tapgyn@gmail.com>
2012-07-09 02:36:31 -0300

    crazy egg, tested. 306 examples, 0 failures, 17 pending

--- spec/helpers/metrics_helper_spec.rb
+++ spec/helpers/metrics_helper_spec.rb
@@ -6,4 +6,9 @@ describe MetricsHelper do
       helper.google_analytics.should be_nil
     end
   end
+  describe "crazy_egg" do
+    it "should return nil in test env" do
+      helper.crazy_egg.should be_nil
+    end
+  end
 end
