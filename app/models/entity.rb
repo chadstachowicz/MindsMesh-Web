049fb7c296b96f8007f6dbcb740447e1a7dc0e68
Thiago Almeida <tapgyn@gmail.com>
2012-07-10 04:33:28 -0300

    forgot to add file

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,7 +1,7 @@
 class Entity < ActiveRecord::Base
   attr_accessible :name, :slug
-  has_many :entity_user_requests
-  has_many :entity_users
-  has_many :topics
+  has_many :entity_user_requests, dependent: :destroy
+  has_many :entity_users, 		  dependent: :destroy
+  has_many :topics, 			  dependent: :destroy
   validates_presence_of :name
 end
