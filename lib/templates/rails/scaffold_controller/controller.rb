ac6bfa290e3db41a311310878120e1e84bac6246
Thiago Almeida <tapgyn@gmail.com>
2012-07-19 03:31:38 -0300

    generating old school responders

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -1,54 +1,142 @@
 <% module_namespacing do -%>
 class <%= controller_class_name %>Controller < ApplicationController
-  respond_to :html#, :json, :xml
-
+  load_and_authorize_resource
+  
+  # TODO: name these comments properly with all the matching URLs to each action
   # GET <%= route_url %>
   def index
-    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
-    respond_with(@<%= plural_table_name %>)
   end
 
   # GET <%= route_url %>/1
   def show
-    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-    respond_with(@<%= singular_table_name %>)
   end
 
   # GET <%= route_url %>/new
   def new
+  end
+
+  # POST <%= route_url %>
+  def create
+    if @<%= orm_instance.save %>
+      redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was created.'" %>
+    else
+      render <%= key_value :action, '"new"' %>
+    end
+  end
+
+  # GET <%= route_url %>/1/edit
+  def edit
+  end
+
+  # PUT <%= route_url %>/1
+  def update
+    if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+      redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was updated.'" %>
+    else
+      render <%= key_value :action, '"edit"' %>
+    end
+  end
+
+  # DELETE <%= route_url %>/1
+  def destroy
+    @<%= orm_instance.destroy %>
+    redirect_to <%= index_helper %>_url
+  end
+
+end
+<% end -%>
+
+=begin
+  # GET <%= route_url %>
+  # GET <%= route_url %>.json
+  def index
+    @<%= plural_table_name %> = <%= orm_class.all(class_name) %>
+
+    #beyond simple html
+    respond_to do |format|
+      format.html {  } # index.html.erb
+      format.js   {  } # index.js.erb
+      format.json { render <%= key_value :json, "@#{plural_table_name}" %> }
+    end
+  end
+
+  # GET <%= route_url %>/1
+  # GET <%= route_url %>/1.json
+  def show
+    @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
+
+    #beyond simple html
+    respond_to do |format|
+      format.html {  } # show.html.erb
+      format.js   {  } # show.js.erb
+      format.json { render <%= key_value :json, "@#{singular_table_name}" %> }
+    end
+  end
+
+  # GET <%= route_url %>/new
+  # GET <%= route_url %>/new.json
+  def new
     @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
-    respond_with(@<%= singular_table_name %>)
+    #beyond simple html, use show code
   end
 
   # GET <%= route_url %>/1/edit
   def edit
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-    respond_with(@<%= singular_table_name %>)
+    #beyond simple html, use show code
   end
 
   # POST <%= route_url %>
+  # POST <%= route_url %>.json
   def create
-    @<%= singular_table_name %> = <%= orm_class.build(class_name) %>
-    if @<%= singular_table_name %>.save
-      flash[:notice] = "<%= human_name %> successfully created."
+    @<%= singular_table_name %> = <%= orm_class.build(class_name, "params[:#{singular_table_name}]") %>
+
+    #beyond simple html
+    respond_to do |format|
+      if @<%= orm_instance.save %>
+        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was created.'" %> }
+        format.js   {  } # create.js.erb
+        format.json { render <%= key_value :json, "@#{singular_table_name}" %>, <%= key_value :status, ':created' %>, <%= key_value :location, "@#{singular_table_name}" %> }
+      else
+        format.html { render <%= key_value :action, '"new"' %> }
+        format.js   {  } # create.js.erb
+        #format.js   { render 'create_fail' } # create_fail.js.erb
+        format.json { render <%= key_value :json, "@#{orm_instance.errors}" %>, <%= key_value :status, ':unprocessable_entity' %> }
+      end
     end
-    respond_with(@<%= singular_table_name %>, location: @<%= singular_table_name %>)
   end
 
   # PUT <%= route_url %>/1
+  # PUT <%= route_url %>/1.json
   def update
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
-    if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
-      flash[:notice] = "<%= human_name %> successfully updated."
+
+    #beyond simple html
+    respond_to do |format|
+      if @<%= orm_instance.update_attributes("params[:#{singular_table_name}]") %>
+        format.html { redirect_to @<%= singular_table_name %>, <%= key_value :notice, "'#{human_name} was updated.'" %> }
+        format.js   {  } # update.js.erb
+        format.json { head :no_content }
+      else
+        format.html { render <%= key_value :action, '"edit"' %> }
+        format.js   {  } # update.js.erb
+        #format.js   { render 'update_fail' } # update_fail.js.erb
+        format.json { render <%= key_value :json, "@#{orm_instance.errors}" %>, <%= key_value :status, ':unprocessable_entity' %> }
+      end
     end
-    respond_with(@<%= singular_table_name %>, location: @<%= singular_table_name %>)
   end
 
   # DELETE <%= route_url %>/1
+  # DELETE <%= route_url %>/1.json
   def destroy
     @<%= singular_table_name %> = <%= orm_class.find(class_name, "params[:id]") %>
     @<%= orm_instance.destroy %>
-    respond_with(@<%= singular_table_name %>)
+
+    #beyond simple html
+    respond_to do |format|
+      format.html { redirect_to <%= index_helper %>_url }
+      format.js   {  } # destroy.js.erb
+      format.json { head :no_content }
+    end
   end
-end
-<% end -%>
+=end
