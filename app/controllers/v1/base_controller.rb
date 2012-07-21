60233b82d23d5c8f81cce9b9c7be6a18d4ba9041
Thiago Almeida <tapgyn@gmail.com>
2012-07-20 22:01:20 -0300

    splitting error logic

--- app/controllers/v1/base_controller.rb
+++ app/controllers/v1/base_controller.rb
@@ -3,7 +3,8 @@ class V1::BaseController < ApplicationController
   protected
 
   def authenticate
+    return render json: {error: {message: "this operation requires access_token", code: 1001}} if params[:access_token].blank?
     @me ||= User.where(id: params[:access_token]).first
-    return render json: {error: {message: "invalid access_token", code: 1}} if @me.nil?
+    return render json: {error: {message: "access_token does not match any users", code: 1002}} if @me.nil?
   end
 end
