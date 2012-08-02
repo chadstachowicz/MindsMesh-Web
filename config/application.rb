7f39a0e9093aeafbc6fc23dad5440404c22c2661
Thiago Almeida <tapgyn@gmail.com>
2012-08-02 01:21:35 -0300

    #sidebar_left (empty)

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Mindsmesh
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1343793335'
+    config.assets.version = '1343881250'
   end
 end
