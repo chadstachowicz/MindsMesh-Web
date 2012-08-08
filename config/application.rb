1e92ee0f0e83ba52e45dfb32576236e21cf863ae
Thiago Almeida <tapgyn@gmail.com>
2012-08-08 13:53:03 -0300

    css fix on IE

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Mindsmesh
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1344298671'
+    config.assets.version = '1344298674'
   end
 end
