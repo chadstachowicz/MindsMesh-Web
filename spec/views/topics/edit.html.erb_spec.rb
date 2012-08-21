018eb45276fda5662430a7feb368c0685c32366d
Thiago Almeida <tapgyn@gmail.com>
2012-08-20 22:50:09 -0300

    master can create topics from scaffold views. 433 examples, 12 failures, 19 pending

--- spec/views/topics/edit.html.erb_spec.rb
+++ spec/views/topics/edit.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "topics/edit" do
   before(:each) do
     @topic = assign(:topic, stub_model(Topic,
-      :entity => nil,
+      :entity => stub_model(Entity, name: "lalala"),
       :name => "MyString",
       :slug => "MyString"
     ))
@@ -14,9 +14,12 @@ describe "topics/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => topics_path(@topic), :method => "post" do
-      assert_select "select#topic_entity_id"
+      assert_select "select#topic_entity_user_id", count: 0
       assert_select "input#topic_name", :name => "topic[name]"
       assert_select "input#topic_slug", :name => "topic[slug]"
+      assert_select "input#topic_title", :name => "topic[title]"
+      assert_select "input#topic_number", :name => "topic[number]"
+      assert_select "input#topic_self_joining", :name => "topic[self_joining]"
     end
   end
 end
