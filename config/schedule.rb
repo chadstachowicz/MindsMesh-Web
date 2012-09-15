704f7a0bc735cb67bf4a152828dd6f687d9a165a
Thiago Almeida <tapgyn@gmail.com>
2012-09-15 19:33:23 +0200

    stalker: cron to start daemon on reboot

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,14 @@
 env :PATH, ENV['PATH']
 set :output, "#{path}/log/cron.log"
 
+job_type :script,  "cd :path && RAILS_ENV=:environment bundle exec ruby script/:task :output"
+
 every 2.minutes do
   rake "when:2minutes"
+end
+
+every :reboot do
+  script "stalker_daemon restart"
 end
 
 # Learn more: http://github.com/javan/whenever
