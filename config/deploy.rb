bf7a5e8c2ae45fed3da5e93e274a2d1ae913eb96
Thiago Almeida <tapgyn@gmail.com>
2012-09-26 09:27:04 +0200

    not restart beanstalkd daemon on deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,6 @@ default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
 
 after "deploy", "deploy:cleanup" # keep only the last 5 releases
-after "deploy", "stalk:restart"
 
 namespace :deploy do
 
