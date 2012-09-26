16397b7654c8c8807b29d65387ec4e460749ac19
Thiago Almeida <tapgyn@gmail.com>
2012-09-26 08:40:17 +0200

    removing when:2minutes

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,10 +3,6 @@ set :output, "#{path}/log/cron.log"
 
 job_type :script,  "cd :path && RAILS_ENV=:environment bundle exec ruby script/:task :output"
 
-every 2.minutes do
-  rake "when:2minutes"
-end
-
 every :reboot do
   script "stalker_daemon restart"
 end
--- lib/tasks/my_whenever.rake
+++
@@ -1,8 +0,0 @@
-namespace :when do
-
-  desc "sample"
-  task '2minutes' => :environment do
-  	Notify.run_all %w(Reply Like Post)
-  end
-
-end
