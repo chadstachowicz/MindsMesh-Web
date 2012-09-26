cd576d7bf486d3027f6ce458bbc5b62c72cdd240
Thiago Almeida <tapgyn@gmail.com>
2012-09-26 15:49:27 +0200

    css fix: bootstrap-sass with font-awesome

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Mindsmesh
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1347850506'
+    config.assets.version = '1347850507'
   end
 end
