aa1cbc292eef9f066031b8e2716308610fdf81ff
Thiago Pinto <tapgyn@gmail.com>
2012-10-09 17:32:24 -0400

    HOTFIX /users assign to entity JS bug

--- config/application.rb
+++ config/application.rb
@@ -62,6 +62,6 @@ module Mindsmesh
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1347850510'
+    config.assets.version = '1347850511'
   end
 end
