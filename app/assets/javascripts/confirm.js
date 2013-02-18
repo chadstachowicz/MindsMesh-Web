c2af0eea7e57087d7345ec62608a5d2956eb8975
Chad Stachowicz <cstachowicz@Chads-iMac.local>
2013-02-18 16:50:13 -0500

    fix email i hope

--- app/assets/javascripts/confirm.js
+++ app/assets/javascripts/confirm.js
@@ -1,4 +1,4 @@
-$("#entity-email form").on("ajax:complete", function(e, data) {
+$("#entity-email form").live("ajax:complete", function(e, data) {
   $('#entity_request_modal .modal-body h2').html(data.responseText);
   $('#entity_request_modal').modal();
 });
