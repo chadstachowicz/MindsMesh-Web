9aa16881ddd50f9665bd82399f5152b0e15c055f
Chad Stachowicz <cstachowicz@Chads-iMac.local>
2013-04-09 23:56:56 -0400

    almost complete front page

--- app/assets/javascripts/frontend.js
+++ app/assets/javascripts/frontend.js
@@ -5,4 +5,18 @@ $("#features_link").click(function() {
      }, 500);
 return false;
  });
+$("#schools_link").on('click',function() {
+     $('html, body').animate({
+         scrollTop: $("#for-schools").offset().top
+     }, 500);
+return false;
+ });
+
 });
+function scrollSchools()
+{
+$('html, body').animate({
+         scrollTop: $("#for-schools").offset().top
+     }, 500);
+return false;
+}
