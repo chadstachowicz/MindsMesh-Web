5a32539a4872de2389be9fdebd22a7596777e300
Chad Stachowicz <cstachowicz@test.ucmesh.com>
2013-06-04 20:54:52 -0400

    here is another test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -35,9 +35,11 @@ class HomeController < ApplicationController
   end
 
   def index
-    cookies['suggest_invites'] = "true" if current_user.last_login_at.nil? || current_user.last_login_at < 20.seconds.ago
-    
-    cookies['suggest_follows'] = "true" if current_user.last_login_at.nil? || current_user.last_login_at < 20.seconds.ago
+    if current_user.last_login_at.nil? || current_user.last_login_at < 20.seconds.ago
+        cookies['suggest_follows'] = "true"
+    elsif current_user.last_login_at.nil? || current_user.last_login_at < 20.seconds.ago
+        cookies['suggest_invites'] = "true"
+    end
 
     @type = 'post'
     @posts = current_user.posts_feed
