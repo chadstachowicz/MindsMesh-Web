4550898ddc5873729db40975d85d600f65c099f4
chadstachowicz <cstachowicz@test.ucmesh.com>
2013-08-26 22:13:37 -0400

    sefsdrsdf

--- app/mailers/my_mail.rb
+++ app/mailers/my_mail.rb
@@ -1,5 +1,5 @@
 class MyMail < ActionMailer::Base
-  default from: "noreply@mindsmesh.com"
+  default from: "welcome@mindsmesh.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
