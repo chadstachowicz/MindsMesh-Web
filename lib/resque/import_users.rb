bf1095d8f1ad59e7255ed2b7247464125eeef992
chadstachowicz <cstachowicz@test.ucmesh.com>
2013-08-28 13:09:00 -0400

    sdfsdf

--- lib/resque/import_users.rb
+++ lib/resque/import_users.rb
@@ -15,21 +15,25 @@ class ImportUsers
     
  def self.perform(chunk)
     chunk.each do |i|
+    puts i[:email]
     user = User.where(:email => i[:email]).first_or_initialize
     user.name = i[:name]
-    if user.save
-        entity = Entity.find_by_email_domain(i[:email])
-        eur = EntityUser.where(:entity_id => entity.id, :user_id => user.id).first_or_initialize
-            if eur.save
-                topic = Topic.where(:number => i[:course_number]).first_or_initialize
-                if i[:teacher] == 'TRUE'
-                    eu = TopicUser.where(:topic_id => topic.id, :user_id => user.id, :role_i => 1).first_or_initialize
-                else
-                    eu = TopicUser.where(:topic_id => topic.id, :user_id => user.id).first_or_initialize
-                end
-            eu.save
-            end
-        end
+    puts "Start: #{user.email}"
+    puts "ID: #{user.id}"
+    puts "Start: #{user.name}"
+    #  if user.save
+    #   entity = Entity.find_by_email_domain(i[:email])
+    #    eur = EntityUser.where(:entity_id => entity.id, :user_id => user.id).first_or_initialize
+    #        if eur.save
+    #            puts "Start: BLARGITY End"
+    #            topic = Topic.where(:number => i[:course_number]).first_or_initialize
+    ###                eu = TopicUser.where(:topic_id => topic.id, :user_id => user.id, :role_i => 1).first_or_initialize
+    #          else
+    #                eu = TopicUser.where(:topic_id => topic.id, :user_id => user.id).first_or_initialize
+    #            end
+    #        eu.save
+    #        end
+    #    end
     end
  end
 
