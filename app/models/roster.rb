5f3fdad89791c87baa018b2bb5e55dca74f76d9a
chadstachowicz <cstachowicz@test.ucmesh.com>
2013-09-03 20:56:32 -0400

    fixing this stuff

--- app/models/roster.rb
+++ app/models/roster.rb
@@ -2,7 +2,7 @@ class Roster < ActiveRecord::Base
     attr_accessible :email, :group_id, :topic_id, :user_id, :role
     
   def self.import(file)
-    job = BackgroundJob.create(:type => "ImportRosters", :status => 'Processing')
+    job = BackgroundJob.create(:name => "ImportRosters", :status => 'Processing')
     n = SmarterCSV.process(file.tempfile.to_path.to_s, {:chunk_size => 100}) do |chunk|
         Resque.enqueue( ImportRosters, chunk, job.id ) # pass chunks of CSV-data to Resque workers for parallel processing
     end
