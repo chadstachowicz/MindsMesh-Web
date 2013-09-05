f369d8728ac4236a786d223cdc172e3da6d1ff3b
chadstachowicz <cstachowicz@test.ucmesh.com>
2013-09-05 11:44:55 -0400

    sdfsdf

--- app/assets/javascripts/confirm.js
+++ app/assets/javascripts/confirm.js
@@ -6,4 +6,8 @@ $("#new_signup form").live("ajax:complete", function(e, data) {
   $('#signup_request_modal .modal-body h2').html(data.responseText);
   $('#signup_request_modal').modal();
 });
+$("#new_signup form").live("ajax:complete", function(e, data) {
+  $('#join_entity_modal .modal-body h2').html(data.responseText);
+  $('#join_entity_modal').modal();
+});
 
