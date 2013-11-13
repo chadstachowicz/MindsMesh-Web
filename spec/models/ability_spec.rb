81fe1311b5abdd91c1ee67849183b590550cb0fe
aarkerio <mmontoya@gmail.com>
2013-11-13 12:41:56 -0600

    Adding spec macro for devise and testing cancan class

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -1,26 +1,29 @@
+
+# MindsMesh, Inc. (c) 2012-2013
+
 require 'spec_helper'
 
 describe Ability do
-  describe 'supplier' do
+  describe 'student' do
     subject do 
-      @user = create :user, :role => 'supplier'
+      @user = create :user, :role => 'student'
       @user.administratable = @me
       @user.save
-      @user_belonging_to_the_same_org = create :user, :role => 'supplier'
+      @user_belonging_to_the_same_org = create :user, :role => 'student'
       @user_belonging_to_the_same_org.administratable = @me
       @user_belonging_to_the_same_org.save
-      @some_other_user = create :user, :role => 'supplier'
-      @some_other_user.administratable = @other_supplier
+      @some_other_user = create :user, :role => 'student'
+      @some_other_user.administratable = @other_student
       @some_other_user.save
       Ability.new @user
     end
 
     describe 'access to entities' do
       before do
-        @me = create :supplier
-        @other_supplier = create :supplier
+        @me = create :student
+        @other_student = create :student
         @client = create :client
-        @agreement = create :agreement, :supplier => @me
+        @agreement = create :agreement, :student => @me
         @product = create :product
         @product.agreements << @agreement
         @product.save
@@ -37,10 +40,10 @@ describe Ability do
       it { subject.must_be_able_to(:create, User.new( administratable_type: @me.class.to_s, administratable_id: @me.id))}
       it { subject.must_not_be_able_to(:read, @some_other_user)}
       it { subject.must_not_be_able_to(:update, @some_other_user)}
-      it { subject.must_not_be_able_to(:create, User.new( administratable_type: @me.class.to_s, administratable_id: @other_supplier.id))}
+      it { subject.must_not_be_able_to(:create, User.new( administratable_type: @me.class.to_s, administratable_id: @other_student.id))}
 
-      it { subject.must_not_be_able_to(:read, @other_supplier)}
-      it { subject.must_not_be_able_to(:update, @other_supplier)}
+      it { subject.must_not_be_able_to(:read, @other_student)}
+      it { subject.must_not_be_able_to(:update, @other_student)}
       it { subject.must_be_able_to(:read, @me)}
       it { subject.must_be_able_to(:update, @me)}
 
@@ -84,7 +87,7 @@ describe Ability do
     describe 'access to entities' do
       before do
         @product = create :product
-        @supplier = create :supplier
+        @student = create :student
         @me = create :client
         @other_client = create :client
         @agreement = create :agreement, :client => @me
@@ -102,7 +105,7 @@ describe Ability do
 
       it { subject.must_be_able_to(:search, @product)}
       it { subject.must_be_able_to(:read, @product)}
-      it { subject.must_be_able_to(:read, @supplier)}
+      it { subject.must_be_able_to(:read, @student)}
       it { subject.must_not_be_able_to(:read, @other_client)}
       it { subject.must_not_be_able_to(:update, @other_client)}
       it { subject.must_be_able_to(:read, @me)}
