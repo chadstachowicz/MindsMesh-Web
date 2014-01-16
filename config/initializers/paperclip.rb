8380cdcf1401c8213831bcf76855c4b2795997d5
chadstachowicz <cstachowicz@test.ucmesh.com>
2014-01-15 22:32:40 -0500

    sdfsdf

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,7 @@ if Settings.env['s3']
   Paperclip::Attachment.default_options.merge!({
     :storage => :s3,
     :bucket => 'mindsmesh.com',
-    :s3_credentials => Settings.env['s3']
+    :s3_credentials => Settings.env['s3'],
+    :s3_protocol => 'https'
   })
 end
